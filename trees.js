9348ecf9e8edd69163f820df8e84719c0f9c37eb
Austin Schey <aschey13@gmail.com>
2020-01-18 16:40:53 -0600

    cleanup gradient

--- trees.js
+++ trees.js
@@ -48,7 +48,7 @@ const alpha = 'FF';
 function setup() {
     createCanvas(windowWidth, windowHeight);
     strokeWeight(thickness);
-    background(bgColor);
+    background(bgColorDark);
     noLoop();
 }
 
@@ -144,42 +144,27 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function colorGradient(start_color, end_color, percent) {
-    // strip the leading # if it's there
-    start_color = start_color.replace(/^\s*#|\s*$/g, '');
-    end_color = end_color.replace(/^\s*#|\s*$/g, '');
- 
-    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
-    if(start_color.length == 3){
-      start_color = start_color.replace(/(.)/g, '$1$1');
-    }
- 
-    if(end_color.length == 3){
-      end_color = end_color.replace(/(.)/g, '$1$1');
-    }
- 
+function getColorInt(hexColorString, pos) {
+    return parseInt(hexColorString.substr(pos * 2, 2), 16);
+}
+
+function hexStringToInts(hexColorString) {
+    // Remove leading #
+    let valsOnly = hexColorString.slice(1, hexColorString.length);
+    return _.range(3).map(i => getColorInt(valsOnly, i));
+}
+
+function getNewColorVal(startVal, colorDiff, percent) {
+    return ((colorDiff * percent) + startVal).toString(16).split('.')[0].padStart(2, '0');
+}
+
+function colorGradient(startColor, endColor, percent) {
     // get colors
-    var start_red = parseInt(start_color.substr(0, 2), 16),
-        start_green = parseInt(start_color.substr(2, 2), 16),
-        start_blue = parseInt(start_color.substr(4, 2), 16);
- 
-    var end_red = parseInt(end_color.substr(0, 2), 16),
-        end_green = parseInt(end_color.substr(2, 2), 16),
-        end_blue = parseInt(end_color.substr(4, 2), 16);
- 
+    let startInts = hexStringToInts(startColor);
+    let endInts = hexStringToInts(endColor);
+
     // calculate new color
-    var diff_red = end_red - start_red;
-    var diff_green = end_green - start_green;
-    var diff_blue = end_blue - start_blue;
- 
-    diff_red = ( (diff_red * percent) + start_red ).toString(16).split('.')[0];
-    diff_green = ( (diff_green * percent) + start_green ).toString(16).split('.')[0];
-    diff_blue = ( (diff_blue * percent) + start_blue ).toString(16).split('.')[0];
- 
-    // ensure 2 digits by color
-    if( diff_red.length == 1 ) diff_red = '0' + diff_red
-    if( diff_green.length == 1 ) diff_green = '0' + diff_green
-    if( diff_blue.length == 1 ) diff_blue = '0' + diff_blue
- 
-    return '#' + diff_red + diff_green + diff_blue;
-  };
+    let newVals = startInts.map((startVal, i) => getNewColorVal(startVal, endInts[i] - startVal, percent));
+
+    return `#${newVals.join('')}`;
+};
