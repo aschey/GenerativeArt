97b72043806868efd2fac9a8d91f7ec59588ca66
Austin Schey <aschey13@gmail.com>
2020-01-18 16:07:27 -0600

    add background

--- trees.js
+++ trees.js
@@ -1,28 +1,48 @@
 /// <reference path="node_modules/@types/p5/global.d.ts" />
 
-const MAX_X = 10;
+// Max x distance from trunk on either side
+const MAX_X = 15;
+// Branches cannot go more than n times higher than the trunk height
 const MIN_Y_FACTOR = 2;
+// Max amount to add/subtract from the x value each time
 const X_VAR = 15;
+// Max amount to subtract from the y value each time
 const Y_VAR = 15;
+// Branches off the trunk can start no less than n * trunkHeight from the base of the trunk
 const MIN_Y_THRESH_FACTOR_BOTTOM = 0.1;
+// Branches off the trunk can start no more than n * trunkHeight from the base of the trunk
 const MAX_Y_THRESH_FACTOR_BOTTOM = 0.5;
+// Branches off the trunk can end no less than n * trunkHeight from the base of the trunk
 const MIN_Y_THRESH_FACTOR_TOP = 0.05;
+// Branches off the trunk can end no more than n * trunkHeight from the base of the trunk
 const MAX_Y_THRESH_FACTOR_TOP = 0.1;
+// Minimum variance from START_X or START_Y to place a new tree
 const PLACE_VAR_MIN = -10;
+// Maximum variance from START_X or START_Y to place a new tree
 const PLACE_VAR_MAX = 20;
+// Minimum trunk height
 const MIN_TRUNK_HEIGHT = 20;
+// Maximum trunk height
 const MAX_TRUNK_HEIGHT = 50;
+// Minimum branches per tree
 const MIN_BRANCHES = 10;
+// Maximum branches per tree
 const MAX_BRANCHES = 25;
+// Starting x coord to place trees
 const START_X = 50;
+// Starting y coord to place trees
 const START_Y = 50;
+// Minimum similarity ratio for branch slopes within the same tree
 const SLOPE_THRESH_MIN = 0.2;
+// Maximum similarity ratio for branch slopes within the same tree
 const SLOPE_THRESH_MAX = 0.5;
+// Minimum amount of distance allowed between branches
 const DIST_THRESH = 4;
 
 const thickness = 2;
-const bgColor = '#101C23'
-const colors = ['#743A15', '#735C20', '#4A583B', '#2D473F', '#393E41']
+const bgColorDark = '#101C23';
+const bgColorLight = '#233641'
+const colors = ['#743A15', '#735C20', '#4A583B', '#2D473F', '#393E41'];
 const alpha = 'FF';
 
 function setup() {
@@ -32,19 +52,34 @@ function setup() {
     noLoop();
 }
 
-async function draw() {
+function draw() {
+    drawBackground();
+
     for (let y = START_Y; y < height; y += START_Y) {
         for (let x = START_X; x < width; x += START_X) {
-            await drawTree(x + random(PLACE_VAR_MIN, PLACE_VAR_MAX), y + random(PLACE_VAR_MIN, PLACE_VAR_MAX), random(colors));
+            drawTree(x + random(PLACE_VAR_MIN, PLACE_VAR_MAX), y + random(PLACE_VAR_MIN, PLACE_VAR_MAX), random(colors));
         }
     }
 }
 
-async function drawTree(x, y, color) {
+function drawBackground() {
+    noiseDetail(5, 0.5);
+    for (let y = 0; y < height; y+=random(2, 5)) {
+        for (let x = 0; x < width; x+=random(2, 5)) {
+            let noiseVal = noise(x * 0.02, y * 0.02);
+            let val = colorGradient(bgColorDark, bgColorLight, noiseVal);
+            stroke(val);
+            point(x, y + random(-noiseVal * 2, noiseVal * 5));
+        }
+    }
+}
+
+function drawTree(x, y, color) {
     stroke(color + alpha);
     let trunkHeight = random(MIN_TRUNK_HEIGHT, MAX_TRUNK_HEIGHT);
+    // draw trunk
     line(x, y, x, y - trunkHeight);
-    iters = 0;
+
     lines = [{x1: x, y1: y, x2: x, y2: y - trunkHeight}];
     numBranches = random(MIN_BRANCHES, MAX_BRANCHES);
     let yThreshFactorBottom = random(MIN_Y_THRESH_FACTOR_BOTTOM, MAX_Y_THRESH_FACTOR_BOTTOM);
@@ -81,7 +116,6 @@ async function drawTree(x, y, color) {
         }
         line(x1, y1, x2, y2);
         lines.push({x1, y1, x2, y2});
-        //await sleep(0.1);
     }
 }
 
@@ -109,3 +143,43 @@ function distance(x1, y1, x2, y2) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+function colorGradient(start_color, end_color, percent) {
+    // strip the leading # if it's there
+    start_color = start_color.replace(/^\s*#|\s*$/g, '');
+    end_color = end_color.replace(/^\s*#|\s*$/g, '');
+ 
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(start_color.length == 3){
+      start_color = start_color.replace(/(.)/g, '$1$1');
+    }
+ 
+    if(end_color.length == 3){
+      end_color = end_color.replace(/(.)/g, '$1$1');
+    }
+ 
+    // get colors
+    var start_red = parseInt(start_color.substr(0, 2), 16),
+        start_green = parseInt(start_color.substr(2, 2), 16),
+        start_blue = parseInt(start_color.substr(4, 2), 16);
+ 
+    var end_red = parseInt(end_color.substr(0, 2), 16),
+        end_green = parseInt(end_color.substr(2, 2), 16),
+        end_blue = parseInt(end_color.substr(4, 2), 16);
+ 
+    // calculate new color
+    var diff_red = end_red - start_red;
+    var diff_green = end_green - start_green;
+    var diff_blue = end_blue - start_blue;
+ 
+    diff_red = ( (diff_red * percent) + start_red ).toString(16).split('.')[0];
+    diff_green = ( (diff_green * percent) + start_green ).toString(16).split('.')[0];
+    diff_blue = ( (diff_blue * percent) + start_blue ).toString(16).split('.')[0];
+ 
+    // ensure 2 digits by color
+    if( diff_red.length == 1 ) diff_red = '0' + diff_red
+    if( diff_green.length == 1 ) diff_green = '0' + diff_green
+    if( diff_blue.length == 1 ) diff_blue = '0' + diff_blue
+ 
+    return '#' + diff_red + diff_green + diff_blue;
+  };
